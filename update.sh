e420d52dcf337f543d2ebd9e2b8b9c7a8e97f287
Natanael Arndt <arndtn@gmail.com>
2021-07-06 20:44:54 +0200

    some stuff

---
+++ update.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+LC_ALL=C
+METADATA=metadata.ttl
+EXPORT=graph_tmp.nt
+GRAPH=graph.nt
+BASE_IRI=https://posters.lswt2021.aksw.org/
+
+MESSAGE="Update ORKG Data"
+LOG=""
+
+LOG="${LOG}"$( date '+%Y-%m-%d %H.%M.%S%z' )"\n"
+
+touch $EXPORT
+echo "" > $EXPORT
+
+# TODO add mapping
+
+i=0
+for dir in _data/poster/*; do
+  i=$i+1
+  dirname=$(basename $dir)
+  echo "<https://posters.lswt2021.aksw.org/$i> <http://www.w3.org/2000/01/rdf-schema#seeAlso> <https://posters.lswt2021.aksw.org/$dirname> ." >> $EXPORT
+  ERROR=$( { rapper -i turtle -o ntriples $dir/$METADATA $BASE_IRI >> $EXPORT; } 2>&1 )
+  LOG="${LOG}${ERROR}\n"
+done
+sort -u $EXPORT > $GRAPH
+LOG="${LOG}"$( wc -l $GRAPH )"\n"
+rm $EXPORT
+
+echo $LOG
+
+for dir in _data/poster/*; do
+  dirname=$(basename $dir)
+  ERROR=$( { pdftocairo -png -scale-to 2000 -singlefile $dir/${dirname}.pdf img/${dirname}_s.png; } 2>&1 )
+  LOG="${LOG}${ERROR}\n"
+  ERROR=$( { pdftocairo -png -scale-to 3000 -singlefile $dir/${dirname}.pdf img/${dirname}_l.png; } 2>&1 )
+  LOG="${LOG}${ERROR}\n"
+done
+
+echo $LOG
+
+# for dir in _data/poster/
+# dirname = localpart(dir)
