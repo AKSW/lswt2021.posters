6ea670e998d759cdd38c9febe7f5ac52f6113f14
Natanael Arndt <arndtn@gmail.com>
2021-07-06 21:38:24 +0200

    update

--- update.sh
+++ update.sh
@@ -18,9 +18,12 @@ echo "" > $EXPORT
 
 i=0
 for dir in _data/poster/*; do
-  i=$i+1
+  i=$((i + 1))
   dirname=$(basename $dir)
+  echo "<https://posters.lswt2021.aksw.org/$i> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://xmlns.com/foaf/0.1/Document> ." >> $EXPORT
   echo "<https://posters.lswt2021.aksw.org/$i> <http://www.w3.org/2000/01/rdf-schema#seeAlso> <https://posters.lswt2021.aksw.org/$dirname> ." >> $EXPORT
+  echo "<https://posters.lswt2021.aksw.org/$dirname> <https://posters.lswt2021.aksw.org/slug> \"$dirname\" ." >> $EXPORT
+  echo "<https://posters.lswt2021.aksw.org/$dirname> <https://posters.lswt2021.aksw.org/number> \"$i\" ." >> $EXPORT
   ERROR=$( { rapper -i turtle -o ntriples $dir/$METADATA $BASE_IRI >> $EXPORT; } 2>&1 )
   LOG="${LOG}${ERROR}\n"
 done
@@ -28,13 +31,13 @@ sort -u $EXPORT > $GRAPH
 LOG="${LOG}"$( wc -l $GRAPH )"\n"
 rm $EXPORT
 
-echo $LOG
+#echo $LOG
 
 for dir in _data/poster/*; do
   dirname=$(basename $dir)
-  ERROR=$( { pdftocairo -png -scale-to 2000 -singlefile $dir/${dirname}.pdf img/${dirname}_s.png; } 2>&1 )
+  ERROR=$( { pdftocairo -png -scale-to 1200 -singlefile $dir/${dirname}.pdf img/${dirname}_s; } 2>&1 )
   LOG="${LOG}${ERROR}\n"
-  ERROR=$( { pdftocairo -png -scale-to 3000 -singlefile $dir/${dirname}.pdf img/${dirname}_l.png; } 2>&1 )
+  ERROR=$( { pdftocairo -png -scale-to 3000 -singlefile $dir/${dirname}.pdf img/${dirname}_l; } 2>&1 )
   LOG="${LOG}${ERROR}\n"
 done
 
